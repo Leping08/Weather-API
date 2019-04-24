068cdcaf80afc83e8a897f058e8a1e811236251e
Derek Schmitt <Derek@Dereks-MBP.localdomain>
2019-04-24 12:25:33 -0400

    Create laravel app

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('api_token')->group(function () {
+
+    Route::post('/weather/{api_token}', function () {
+        Log::info("Temp: " . request()->temp);
+        Log::info("Humidity: " . request()->humidity);
+        Log::info("Pressure: " . request()->pressure);
+    });
+
+});
+
