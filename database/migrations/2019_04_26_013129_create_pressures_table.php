2d0df47ec26e344c365d79192b55621bdb94619d
Derek Schmitt <Derek@Dereks-MacBook-Pro.local>
2019-04-25 21:37:42 -0400

    Add Pressure model and add measurement_time column to humidities table and temperatures table

---
+++ database/migrations/2019_04_26_013129_create_pressures_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePressuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pressures', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('millibars');
+            $table->dateTime('measurement_time');
+            $table->timestamps();
+        });
+
+        Schema::table('humidities', function (Blueprint $table){
+            $table->dateTime('measurement_time')->after('percentage');
+        });
+
+        Schema::table('temperatures', function (Blueprint $table){
+            $table->dateTime('measurement_time')->after('degrees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pressures');
+
+        Schema::table('humidities', function (Blueprint $table) {
+            $table->dropColumn('measurement_time');
+        });
+
+        Schema::table('temperatures', function (Blueprint $table) {
+            $table->dropColumn('measurement_time');
+        });
+    }
+}
